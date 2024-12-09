d283c8d5b32994051545d5e1c25cb6c7cab64c98
phessophissy <73696997+phessophissy@users.noreply.github.com>
2024-12-09 11:09:58 +0100

    Update viewQuery.js

--- src/queries/viewQuery.js
+++ src/queries/viewQuery.js
@@ -1,2 +1,18 @@
+const { setupBrowser } = require('../utils/browserSetup');
+require('dotenv').config();
 
-## Examples
+async function queryView(viewId) {
+    let browser;
+    try {
+        browser = await setupBrowser();
+        const page = await browser.newPage();
+        // ... rest of your query code ...
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    } finally {
+        if (browser) await browser.close();
+    }
+}
+
+module.exports = { queryView };
